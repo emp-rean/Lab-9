c6fd2ce99b154348aa035e8ec5d550a3114d996f
emp-rean <spforimport@gmail.com>
2025-03-30 18:09:18 +0200

    54

--- Lab-9.cpp
+++ Lab-9.cpp
@@ -9,11 +9,12 @@ using std::hex;
 
 int main()
 {
-    long long n;
+    long long n; // (8 байт = 64 бита)
     cout << "Enter number: ";
     cin >> n;
-    unsigned char* c = (unsigned char *)&n;
-    for (int i = 0; i < sizeof(n); ++i) {
+    unsigned char* c = (unsigned char *)&n; // адрес переменной n в чар массив из 8 байт
+    for (int i = 0; i < sizeof(n); ++i) //8 раз для каждого байта
+    {
         cout << "Byte " << i << ": " << hex << (int)c[i] << endl;
     }
 }
